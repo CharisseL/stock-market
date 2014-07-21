0da6c583d062930daeda83228735d2f2c112f78b
Charisse Lambert <charisse8@gmail.com>
2014-07-21 10:54:10 -0500

    Stock constructor

---
+++ test/unit/stock.js
@@ -0,0 +1,20 @@
+//test file
+
+/*global describe, it */
+'use strict';
+
+var expect = require('chai').expect;
+var Stock = require('../../app/models/stock.js');
+
+describe('Stock', function(){
+  describe('constructor', function(){
+    it('should create a new stock object', function(){
+      var aapl = new Stock('aApL', '100');
+
+      expect(aapl).to.be.instanceof(Stock);
+      expect(aapl.symbol).to.equal('AAPL');
+      expect(aapl.count).to.equal(100);
+      expect(aapl.price).to.equal(0);
+    });
+  });
+});
