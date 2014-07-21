da8bf9dfd3ad9979c3eda241672200240108d761
Charisse Lambert <charisse8@gmail.com>
2014-07-21 14:29:21 -0500

    created Portfolio #add

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,8 +1,33 @@
 'use strict';
 
+var Stock = require('./stock');
+
 function Portfolio(name, stocks){
   this.name = name;
   this.stocks = [];
+}
+
+Portfolio.prototype.add = function(symbol, amount){
+  var stock = findStock(this.stocks, symbol);
+  
+  if(stock){ //if you find this stock, modify it by adding stocks together
+    stock.count += amount;
+  }else{ //if not, it will create a new object & push new object into array
+    stock = new Stock(symbol, amount);
+    this.stocks.push(stock);
+  }
+}; 
+
+// PRIVATE HELPER FUNCTIONS//
+// this function is "hidden", and is not exported with the Portfolio model
+
+function findStock(stocks, symbol){
+  for(var i = 0; i < stocks.length; i++){
+    if(stocks[i].symbol === symbol.toUpperCase()){
+      return stocks[i];
+    }
+  }
+  return null;
 }
 
 
--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -5,6 +5,7 @@
 
 var expect = require('chai').expect;
 var Portfolio = require('../../app/models/portfolio.js');
+var Stock = require('../../app/models/stock.js');
 
 describe('Portfolio', function(){
   describe('constructor', function(){
@@ -14,6 +15,20 @@ describe('Portfolio', function(){
       expect(tech.name).to.equal('Tech Portfolio');
       expect(tech.stocks).to.have.length(0);
     });
+  });
+
+  describe('#add', function(){
+   it('should add stocks objects to the tech portfolio stock array', function(){
+    var tech = new Portfolio('Tech Portfolio');
+    tech.add('aapl', 50); //adds new stock (symbol, and amount of stocks you want to buy)
+    tech.add('amzn', 35); // ^
+    tech.add('aapl', 25); // ^^
+
+    expect(tech.stocks).to.have.length(2);
+    expect(tech.stocks[0]).to.be.instanceof(Stock);
+    expect(tech.stocks[0].count).to.equal(75);
+    expect(tech.stocks[1].count).to.equal(35);
+   });
   });
 });
 
